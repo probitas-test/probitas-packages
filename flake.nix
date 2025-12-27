c6ca476da0aba41206c7da47b804f079a02fec64
Alisue <lambdalisue@gmail.com>
2025-12-27 13:31:20 +0900

    chore: Add Nix to manage development environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Probitas Packages development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            deno
+          ];
+
+          shellHook = ''
+            echo "Entering Probitas Packages development environment"
+          '';
+        };
+      }
+    );
+}
